a427700ff6cfa48d7401db14bb4da20cc0f34836
CsabaMaruszki <csabamaruszki@gmail.com>
2022-12-10 19:18:44 +0100

    input refactor

--- common/stringExpansion.ts
+++ common/stringExpansion.ts
@@ -1,9 +1,17 @@
 interface String {
   splitToArray(by: string): Array<string>;
+  halve(): Array<string>;
 }
 
 String.prototype.splitToArray = function (by: string): Array<string> {
   const arr: Array<string> = this.split(by);
   arr.forEach((el, i, arr) => (arr[i] = el.trim()));
+  return arr;
+};
+
+String.prototype.halve = function (): Array<string> {
+  const arr: Array<string> = new Array<string>();
+  arr.push(this.trim().slice(0, this.length / 2));
+  arr.push(this.trim().slice(this.length / 2));
   return arr;
 };
